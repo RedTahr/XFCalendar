35a3ba5426cfc72bb803a9d07599243706111aa6
RedTahr <redtahr@users.noreply.github.com>
2016-11-04 11:25:36 +1300

    prep dummy data

---
+++ XFCalendar/Model/Session.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace XFCalendar.Model
+{
+	public class Session
+	{
+		public Session ()
+		{
+		}
+
+		public string Title { get; set; }
+		public string Detail { get; set; }
+		public DateTime Begin { get; set; }
+		public DateTime Finish { get; set; }
+	}
+}
--- XFCalendar/XFCalendarViewModel.cs
+++ XFCalendar/XFCalendarViewModel.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+
 namespace XFCalendar
 {
 	public class XFCalendarViewModel
 	{
+
+		public ObservableCollection<Model.Session> Sessions = new ObservableCollection<Model.Session> {
+			new Model.Session{ Title = "Breakfast", Begin = DateTime.Parse("2012-10-10 08:00"), Finish = DateTime.Parse("2012-10-10 09:00") },
+			new Model.Session{ Title = "Keynote", Begin = DateTime.Parse("2012-10-10 09:00"), Finish = DateTime.Parse("2012-10-10 09:50") },
+			new Model.Session{ Title = "Morning Tea", Begin = DateTime.Parse("2012-10-10 10:00"), Finish = DateTime.Parse("2012-10-10 11:00") },
+			new Model.Session{ Title = "Labs", Begin = DateTime.Parse("2012-10-10 11:00"), Finish = DateTime.Parse("2012-10-10 12:00") },
+			new Model.Session{ Title = "Siesta", Begin = DateTime.Parse("2012-10-10 13:00"), Finish = DateTime.Parse("2012-10-10 13:50") },
+			new Model.Session{ Title = "Afternoon Tea", Begin = DateTime.Parse("2012-10-10 14:00"), Finish = DateTime.Parse("2012-10-10 15:00") },
+		};
+
 		public XFCalendarViewModel ()
 		{
 		}
